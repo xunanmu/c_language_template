c3788b9e25e82014c1bce5c7bdd6ff6fa1d57b68
xunanmu <xunanmu@qq.com>
2020-03-14 21:20:05 +0800

    v1.0

---
+++ cmake-build-debug/CMakeFiles/template.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/template.dir/main.c.obj"
+  "libtemplate.dll.a"
+  "template.exe"
+  "template.exe.manifest"
+  "template.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/template.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/test.c.obj"
+  "libtest.dll.a"
+  "test.exe"
+  "test.exe.manifest"
+  "test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
